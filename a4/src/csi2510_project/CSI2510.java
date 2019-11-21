e89125b7a21ca5952ed905dd2f68f195e4eeec80
Ben Herweyer <benjamin.herweyer@gmail.com>
2019-11-21 15:23:25 -0500

    autoformat

--- a4/src/csi2510_project/CSI2510.java
+++ a4/src/csi2510_project/CSI2510.java
@@ -11,14 +11,11 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import javafx.util.Pair;
 
 /**
- *
  * @author Administrator
  */
 public class CSI2510 {
