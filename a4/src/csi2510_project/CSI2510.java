90d6be4152a166008f2c1b2c57a4b00491968910
Ben Herweyer <benjamin.herweyer@gmail.com>
2019-11-29 14:43:57 -0500

    Fix compilation by removing bad imports in CSI2510

--- a4/src/csi2510_project/CSI2510.java
+++ a4/src/csi2510_project/CSI2510.java
@@ -11,11 +11,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import javafx.util.Pair;
 
 /**
  *
